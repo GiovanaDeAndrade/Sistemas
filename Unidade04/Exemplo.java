841ea977a9159f584dc1bdd5c0088c36a4fa6777
Giovana Marcela <giovanaandrade008@gmail.com>
2026-03-21 18:43:17 -0300

    update

--- Unidade04/Exemplo.java
+++ Unidade04/Exemplo.java
@@ -5,31 +5,31 @@ import java.util.Scanner;
 public class Exemplo {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
 
-        System.out.println("Vamos validar se você tem direito a meia entrada");
+        try (Scanner sc = new Scanner(System.in)) {
 
-        System.out.println("Digite sua idade: ");
-        int idade = sc.nextInt();
+            System.out.println("Vamos validar se você tem direito a meia entrada");
 
-        System.out.println("Você doa sangue?");
+            System.out.print("Digite sua idade: ");
+            int idade = sc.nextInt();
 
-        String resposta = sc.next();
-        boolean doador = false;
-        
-        if (resposta.equalsIgnoreCase("Sim")
-            || resposta.equalsIgnoreCase("S")){
-        doador = true;
-                
-        }
+            System.out.print("Você doa sangue?");
 
+            String resposta = sc.next();
 
-        if (idade < 18 || idade >= 60 || doador){
-            System.out.println("Tem direito!");
-        }
-        else { 
-            System.out.println("Não tem direito");
+            boolean doador = false;
+
+            resposta.toUpperCase();
+
+            if (resposta.equalsIgnoreCase("S") || resposta.equalsIgnoreCase("Sim")) {
+                doador = true;
+            }
+
+            if (idade < 18 || idade >= 60 || doador) {
+                System.out.println("Tem direito!");
+            } else {
+                System.out.println("Não tem direito");
+            }
         }
     }
-
 }
