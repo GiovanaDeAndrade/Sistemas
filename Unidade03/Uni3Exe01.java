df301cc7a46715f98061621e1e8c4365b06c3ded
Giovana Marcela <giovanaandrade008@gmail.com>
2026-03-21 18:45:52 -0300

    update

--- Unidade03/Uni3Exe01.java
+++ Unidade03/Uni3Exe01.java
@@ -7,43 +7,35 @@ public class Uni3Exe01 {
     private int largura;
     private int comprimento;
 
-public Uni3Exe01 (int largura , int comprimento){
-    this.largura = largura;
-    this.comprimento = comprimento;
+    public Uni3Exe01(int largura, int comprimento) {
+        this.largura = largura;
+        this.comprimento = comprimento;
+    }
+
+    public int CalcularArea() {
+        return this.largura * this.comprimento;
+
+    }
+
+    public static void main(String[] args) {
+
+        int largura, comprimento;
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.println("Informe a largura do terreno: ");
+            largura = sc.nextInt();
+
+            System.out.println("Informe a largura do comprimentoi: ");
+            comprimento = sc.nextInt();
+
+            Uni3Exe01 uni3Exe01 = new Uni3Exe01(largura, comprimento);
+
+            int area = uni3Exe01.CalcularArea();
+
+            System.out.println("A area total é: " + area);
+
+        }
+    }
+
 }
-
-public int CalcularArea(){
-    return this.largura * this.comprimento;
-
-}
-
-public static void main(String[] args) {
-
-    int largura, comprimento;
-
-    Scanner sc = new Scanner(System.in);
-
-    System.out.println("Informe a largura do terreno: ");
-    largura = sc.nextInt();
-
-    System.out.println("Informe a largura do comprimentoi: ");
-    comprimento = sc.nextInt();
-
-    Uni3Exe01 uni3Exe01 = new Uni3Exe01 (largura, comprimento);
-
-    int area = uni3Exe01.CalcularArea();
-    
-    System.out.println("A area total é: " + area);
-
-    sc.close();
-    
-}
-
-}
-
-
-
-
-
-    
-
