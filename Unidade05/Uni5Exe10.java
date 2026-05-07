d6f141646ac1faec22b6bda9316070e050747654
Giovana Marcela de Andrade <giovanamarcela@furb.br>
2026-05-07 19:54:41 -0300

    upd

---
+++ Unidade05/Uni5Exe10.java
@@ -0,0 +1,19 @@
+public class Uni5Exe10 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10000; i++) {
+            for (int j = 0; j <= 10000; j++) {
+//System.out.println(i + "" + j + "" + (int) Math.pow(soma, 2))
+                int soma = i + j;
+                String termo1 = i + "" + j;
+                String termo2 = "" + (int) Math.pow(soma, 2);
+                if (termo1.equals(termo2)) {
+                    System.out.println(termo1 + " - " + termo2);
+                }
+
+            }
+
+        }
+    }
+
+}
