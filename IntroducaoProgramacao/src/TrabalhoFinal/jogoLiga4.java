8629d671d9a2337e2d3db4ceb03e2d0484e0156f
Giovana Marcela de Andrade <giovanamarcela@furb.br>
2026-06-15 19:32:46 -0300

    up

---
+++ IntroducaoProgramacao/src/TrabalhoFinal/jogoLiga4.java
@@ -0,0 +1,41 @@
+package TrabalhoFinal;
+
+import java.util.Scanner;
+
+public class jogoLiga4 {
+
+    int tamanhoColuna = 7;
+    int tamanhoLinha = 6;
+    private Scanner sc = new Scanner(System.in);
+    private int[] coluna = new int [tamanhoColuna];
+    private int[] linha = new int [tamanhoLinha];
+    int opcao = 0;
+
+
+    do 
+        {System.out.println("--- Menu do Jogo ---");
+        System.out.println("1 - Escolher Cor");
+        System.out.println("2 - Escolher coluna");
+        System.out.println("3 - Mostrar o tabuleiro");
+        System.out.println("3 - Mostrar o tabuleiro");
+        
+        case 1:
+
+
+
+
+
+        } while (opcao != 0);
+
+
+    public int Tabuleiro(){
+        for(int i = 0; i < 6; i++){
+            System.out.println("Coluna " + coluna[i] + "[ " + "]");
+
+            for(i = 0; i <7; i++);
+
+
+        }
+    }
+
+}
