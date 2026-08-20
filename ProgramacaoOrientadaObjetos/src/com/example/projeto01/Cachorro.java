793a95e49ef78fd786285114687bc7879cec58f0
Giovana de Andrade <giovanaandrade008@gmail.com>
2026-08-20 19:22:40 -0300

    feat: adiciona projeto Eclipse de POO 2026.2

---
+++ ProgramacaoOrientadaObjetos/src/com/example/projeto01/Cachorro.java
@@ -0,0 +1,10 @@
+package com.example.projeto01;
+
+public class Cachorro {
+    String raca;
+    String nome;
+
+    public void latir() {
+        System.out.println("auauau");
+    }
+}
---
+++ ProgramacaoOrientadaObjetos/src/com/example/projeto01/Carro.java
@@ -0,0 +1,10 @@
+package com.example.projeto01;
+
+public class Carro {
+    String marca;
+    int kmRodado;
+
+    public void andar(int km) {
+        kmRodado = kmRodado + km;
+    }
+}
